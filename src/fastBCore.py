6361ea98ada6c16ad9f1e13283c1105b860b8a97
joyantabasak13 <joyantabasak13@gmail.com>
2021-11-02 19:09:11 +0600

    DSL Added
    Signed-off-by: joyantabasak13 <joyantabasak13@gmail.com>

--- src/fastBCore.py
+++ src/fastBCore.py
@@ -68,25 +68,32 @@ def clear_labeling(graph):
         d.clear()
 
 
-def dsl_util(graph, k, root, paths, node, visited, metapath, val, path, counter):
+def dsl_util(graph, k, root, paths, node, visited, visited_edges, metapath, val, path, counter):
     visited.add(node)
     path.append(node)
 
     if counter >= len(metapath):
-        val = val + 1
-        if root in paths:
-            paths[root].append(path.copy())
+        if root != node:
+            val = val + 1
+            if root in paths:
+                paths[root].append(path.copy())
+            else:
+                paths[root] = []
+                paths[root].append(path.copy())
+            path.pop()
+            return val, paths
         else:
-            paths[root] = path.copy()
-        path.pop()
-        return val, paths
+            path.pop()
+            return val, paths
     if val >= k:
         path.pop()
         return val, paths
     for neighbour in graph.neighbors(node):
-        if neighbour not in visited:
+        edge_tuple = (node, neighbour)
+        if (neighbour not in visited) or (edge_tuple not in visited_edges):
             if graph.nodes[neighbour]['type'] == metapath[counter]:
-                val, paths = dsl_util(graph, k, root, paths, neighbour, visited, metapath, val, path, counter + 1)
+                visited_edges.add(edge_tuple)
+                val, paths = dsl_util(graph, k, root, paths, neighbour, visited, visited_edges, metapath, val, path, counter + 1)
     path.pop()
     return val, paths
 
@@ -96,15 +103,15 @@ def dsl(graph, queryNSet, metapath, k_core):
     node_paths = dict()
     for node in queryNSet:
         visited = set()
+        visited_edges = set()
         paths = dict()
-        val, paths = dsl_util(graph, k_core, node, paths, node, visited, metapath, 0, [], 1)
+        val, paths = dsl_util(graph, k_core, node, paths, node, visited, visited_edges, metapath, 0, [], 1)
         if val < k_core:
             low_k_nodes.add(node)
+        if node in node_paths:
+            node_paths[node].append(paths)
         else:
-            if node in node_paths:
-                node_paths[node].append(paths)
-            else:
-                node_paths[node] = paths
+            node_paths[node] = paths
     print("DSL Results")
     print(low_k_nodes)
     print(node_paths.keys())
@@ -114,9 +121,72 @@ def dsl(graph, queryNSet, metapath, k_core):
     return low_k_nodes, node_paths
 
 
+def find_new_path_util(graph, visited, root, node, metapath, path, counter, forbidden_destination, existing_paths,
+                       has_found_new_path):
+    if has_found_new_path:
+        return has_found_new_path
+    visited.add(node)
+    path.append(node)
+    if counter >= len(metapath):
+        if (node != forbidden_destination) and (path not in existing_paths):
+            has_found_new_path = True
+            return has_found_new_path
+        path.pop()
+        return has_found_new_path
+
+    for neighbour in graph.neighbors(node):
+        if neighbour not in visited:
+            if graph.nodes[neighbour]['type'] == metapath[counter]:
+                has_found_new_path = find_new_path_util(graph, visited, root, neighbour, metapath, path,
+                                                               counter + 1, forbidden_destination, existing_paths,
+                                                               has_found_new_path)
+                if has_found_new_path:
+                    return has_found_new_path
+    if not has_found_new_path:
+        path.pop()
+    return has_found_new_path
+
+
+def find_new_path(graph, source, metapath, forbidden_destination, existing_paths):
+    visited = set()
+    path = []
+    has_found_new_path = find_new_path_util(graph, visited, source, source, metapath, path, 1, forbidden_destination,
+                                            existing_paths, False)
+    return has_found_new_path, path
+
+
+def kcore_refinement(graph, low_k_nodes, node_paths, S, metapath, k_core):
+    while len(low_k_nodes) > 0:
+        v = low_k_nodes.pop()
+        if v in S:
+            S.remove(v)
+        else:
+            continue
+        U = []
+        path_v_u = dict()
+
+        for path in node_paths[v][v]:
+            U.append(path[len(metapath) - 1])
+            path_v_u[path[len(metapath) - 1]] = path
+
+        for u in U:
+            if path_v_u[u] is not None:
+                if list(reversed(path_v_u[u])) in node_paths[u][u]:
+                    node_paths[u][u].remove(list(reversed(path_v_u[u])))
+
+                if len(node_paths[u][u]) < k_core:
+                    has_found_new_path, new_path = find_new_path(graph, u, metapath, v, node_paths[u][u])
+                    if has_found_new_path:
+                        node_paths[u][u].append(new_path)
+                    else:
+                        low_k_nodes.add(u)
+
+    return S
+
+
 if __name__ == '__main__':
     graph, query, metapath, k_core = take_inputs()
-    k_core = 5
+    k_core = 3
     nx.draw(graph, with_labels=True)
     plt.savefig("sampleGraph.png")
     if query != 0:
@@ -126,3 +196,6 @@ if __name__ == '__main__':
         print("S is {}".format(S))
         clear_labeling(graph)
         low_k_nodes, node_paths = dsl(graph, S, metapath, k_core)
+        print("Low K Nodes {}".format(low_k_nodes))
+        kcore_vertices = kcore_refinement(graph, low_k_nodes, node_paths, S, metapath, k_core)
+        print("Final Candidates: {}".format(kcore_vertices))
